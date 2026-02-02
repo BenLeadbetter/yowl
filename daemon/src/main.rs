792440d046791de4592daf64cdf77feb9c565fc6
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2026-02-02 10:53:31 +0100

    feat(daemon): log heartbeat

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,7 +1,14 @@
 mod logging;
 
+use std::thread;
+use std::time::Duration;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     crate::logging::init()?;
-    println!("Hello, world!");
-    Ok(())
+    log::info!("yowl daemon started");
+
+    loop {
+        thread::sleep(Duration::from_secs(3));
+        log::info!("heartbeat");
+    }
 }
